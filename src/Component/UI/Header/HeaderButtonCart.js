a862e60b7e3b1857fe5c701179f5bf896a5cb5f0
kunalkhairnar1198 <kunalkhairnar61@gmail.com>
2024-02-28 14:05:14 +0530

    update btn iconadd CartIcon

--- src/Component/UI/Header/HeaderButtonCart.js
+++ src/Component/UI/Header/HeaderButtonCart.js
@@ -1,9 +1,15 @@
 import React from "react"
-import cartBtn from './HeaderButtonCart.css'
-const HeaderButtonCart = (props) => {
+import cartBtn from './HeaderButtonCart.module.css'
+import CartIcon from "../../Cart/CartIcon";
+
+const HeaderButtonCart = () => {
   return (
-    <button className={cartBtn.btn}>
-        Your Cart
+    <button className={cartBtn.button}>
+      <span className={cartBtn.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={cartBtn.badge}>0</span>
     </button>
   )
 };
